100e55310bc235a16769da22930587b9de96dbcc
Aditya Saxena <adityasaxena914@gmail.com>
2025-04-05 22:17:44 +0530

    Fixed Bugs

--- script.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.getElementById("theme-toggle");
-const root = document.documentElement; // <html>
-
-toggleBtn.addEventListener("click", () => {
-  root.classList.toggle("dark");
-  toggleBtn.textContent = root.classList.contains("dark") ? "🌙" : "🌞";
-});
